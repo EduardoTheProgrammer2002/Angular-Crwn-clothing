206ae51f9f62ba5da31f6c53198719ec11a73bb9
Eduardo Rodriguez <edrodriguez@intellisys.com.do>
2022-05-23 10:17:33 -0700

    This creates the visible property for the modal service and uses it to show the modal, also creates a method to handle the toggle of visible prop.

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -4,6 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ModalService {
+  private visible:boolean = false
 
   constructor() { }
+
+  isModalOpen() {
+    return this.visible;
+  }
+
+  toggleModal() {
+    this.visible = !this.visible;
+  }
 }
