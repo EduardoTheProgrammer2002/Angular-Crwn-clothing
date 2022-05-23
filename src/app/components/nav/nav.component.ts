74671e2ae2552528e11732ff9e3b156934af9257
Eduardo Rodriguez <edrodriguez@intellisys.com.do>
2022-05-23 10:47:49 -0700

    Using modal service in the nav component to show and close the modal through using  methods defined earlier.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,12 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modal: ModalService) { }
 
   ngOnInit(): void {
   }
 
   clck() {
     console.log('click');
+  }
+
+  openModal(event: Event) {
+    event.preventDefault();
+    this.modal.toggleModal();
   }
 }
