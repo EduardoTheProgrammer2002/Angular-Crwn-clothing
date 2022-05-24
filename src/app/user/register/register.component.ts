551635bda1da25c30244830712e070dfa732f13b
Eduardo Rodriguez <edrodriguez@intellisys.com.do>
2022-05-24 10:34:25 -0700

    Registering a Form with angular FormGroup and FormControl.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ]);
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(13),
+    Validators.max(90)
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ]);
+  confirm_password = new FormControl('', [
+    Validators.required
+  ]);
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password
+  })
 
 }
