5a80e0f2554e148a1f60e3250d0d055e585a19cd
Eduardo Rodriguez <edrodriguez@intellisys.com.do>
2022-07-25 09:39:37 -0700

    This accesses to the authState property in the nav component to show the sign in button when a user is not logged in, otherwise show the sign out button in the nav component.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
+import { StorageService } from 'src/app/services/storageService/storage.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,10 +8,16 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
-  constructor(private modal: ModalService) { }
+  authState: boolean = false;
+  constructor(
+    private modal: ModalService,
+    private storage: StorageService
+  ) { }
 
   ngOnInit(): void {
+    this.storage.authState.subscribe(val => {
+      this.authState = val;
+    })
   }
 
   clck() {
