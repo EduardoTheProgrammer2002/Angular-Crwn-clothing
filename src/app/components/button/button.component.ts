fec6a840afb63b4e6d93d3d75bf2a188f136a794
Eduardo Rodriguez <edwardo16blak@gmail.com>
2022-08-15 11:46:06 -0700

    This adds a disabled property to the button component to disable the button in the cart component.

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() buttonType: string = "";
+  @Input() disabled: boolean = false;
 
   constructor() { }
 
