8c69483284fc106f892cdfa91de62ced472396e5
Eduardo Rodriguez <edwardo16blak@gmail.com>
2022-09-20 16:02:25 -0700

    This implements the removeItem method in the checkout page.

--- src/app/components/checkout-item/checkout-item.component.ts
+++ src/app/components/checkout-item/checkout-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IItem } from 'src/app/interfaces/Iitems';
 import { faTrash, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { ItemService } from 'src/app/services/itemService/item.service';
+import { StorageService } from 'src/app/services/storageService/storage.service';
 
 @Component({
   selector: 'app-checkout-item',
@@ -15,10 +17,27 @@ export class CheckoutItemComponent implements OnInit {
     price: '120'
   }
   trashbin: IconDefinition = faTrash;
+  token: (string| null) = this.storage.token;
 
-  constructor() { }
+  constructor(
+    private Item: ItemService,
+    private storage: StorageService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  removeItem(item:IItem) {
+    //removing the item
+    return this.Item.deleteItem(item, this.token?? '').subscribe((res) => {
+      const response:any = res;
+      if (!response.ok) {
+        console.error(response.err);
+        return;
+      }
+      this.storage.removeItem(item);
+      this.storage.decreaseQuantity(Number(item.quantity));
+    });
   }
 
 }
