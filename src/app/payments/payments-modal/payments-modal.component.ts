a611cc086fe7cd3f088ab015f8313895a4781cc6
Eduardo Rodriguez <edwardo16blak@gmail.com>
2022-09-26 11:40:38 -0700

    This adds a payments-modal component to the payments module.

---
+++ src/app/payments/payments-modal/payments-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payments-modal',
+  templateUrl: './payments-modal.component.html',
+  styleUrls: ['./payments-modal.component.scss']
+})
+export class PaymentsModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/payments/payments.module.ts
+++ src/app/payments/payments.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaymentsComponent } from './payments/payments.component';
+import { PaymentsModalComponent } from './payments-modal/payments-modal.component';
+import { ComponentsModule } from '../components/components.module';
 
 
 
 @NgModule({
   declarations: [
-    PaymentsComponent
+    PaymentsComponent,
+    PaymentsModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule
   ]
 })
 export class PaymentsModule { }
